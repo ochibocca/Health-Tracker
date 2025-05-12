af089e09eff749366207e61794207214be5486be
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2025-05-12 15:37:23 +0300

    Changed react to vite

---
+++ fronted/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <div>
+        <ul>
+          <li>
+            <a href= "#"> Homepage</a>
+             <a href= "#"> About</a>
+              <a href= "#"> Services</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    </>
+  )
+}
+
+export default App
