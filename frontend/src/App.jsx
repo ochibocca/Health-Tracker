6445011917c4d86235a3a98e615aeefa04ca0672
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2025-05-21 09:23:30 +0300

    added the tracking page and its css page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./pages/home"; 
+import Home from "./pages/home";
+import Track from "./pages/track" 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/track" element={<Track />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/track.jsx
@@ -0,0 +1,25 @@
+import "../pages-css/track.css"
+function Track(){
+    return(
+        <div className="cointainer">
+            <div className="header">
+                <div>
+                    <p>Track</p>
+                    <p>View</p>
+                    <p>History</p>
+                </div>
+            </div>
+            <div className="sidepanel">
+                  <p>Track</p>
+                    <p>View</p>
+                    <p>History</p>
+            </div>
+            <div className="trackhistory">
+                  <p>Track</p>
+                    <p>View</p>
+                    <p>History</p>
+            </div>
+        </div>
+    )
+}
+export default Track
